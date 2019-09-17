7b724f93aeaeba6bd8b685a2e84fc1baea2d734a
vezone <vezone@yandex.com>
2019-09-17 17:10:15 +0400

    Init Project

---
+++ EpamPractice/src/EntryPoint.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace EpamPractice
+{
+    class EntryPoint
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("It works!");
+        }
+    }
+}
